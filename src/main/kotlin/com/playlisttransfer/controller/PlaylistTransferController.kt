658cd0d4a232684af50f5fa434aed449f33b7111
noel-pena <azulu88@hotmail.com>
2024-11-14 17:40:07 -0500

    troubleshooting post return

--- src/main/kotlin/com/playlisttransfer/controller/PlaylistTransferController.kt
+++ src/main/kotlin/com/playlisttransfer/controller/PlaylistTransferController.kt
@@ -1,15 +1,18 @@
-package com.playlisttransfer.controller
-
-import com.playlisttransfer.service.PlaylistTransferService
 import org.springframework.web.bind.annotation.*
+import com.playlisttransfer.service.PlaylistTransferService
+import com.playlisttransfer.model.youtube.YouTubePlaylistItem
 
 @RestController
 @RequestMapping("/api/playlist")
 class PlaylistTransferController(private val playlistTransferService: PlaylistTransferService) {
 
     @PostMapping("/transfer")
-    suspend fun transferPlaylist(@RequestBody playlistUrl: String): String {
-        playlistTransferService.transferPlaylist(playlistUrl)
-        return "Transferring $playlistUrl"
+    suspend fun transferPlaylist(@RequestBody playlistUrl: String): List<YouTubePlaylistItem> {
+        println("Received request to transfer playlist: $playlistUrl")
+
+        val items = playlistTransferService.transferPlaylist(playlistUrl)
+
+        println("Response from PlaylistTransferService: $items")
+        return items
     }
 }
--- src/main/kotlin/com/playlisttransfer/service/PlaylistTransferService.kt
+++ src/main/kotlin/com/playlisttransfer/service/PlaylistTransferService.kt
@@ -1,19 +1,21 @@
 package com.playlisttransfer.service
 
+import com.playlisttransfer.model.youtube.YouTubePlaylistItem
 import org.springframework.stereotype.Service
 
 @Service
 class PlaylistTransferService(private val youtubeService: YouTubeService) {
 
-    suspend fun transferPlaylist(playlistUrl: String) {
-        if (playlistUrl.contains("music.youtube.com")) {
+    suspend fun transferPlaylist(playlistUrl: String): List<YouTubePlaylistItem> {
+        return if (playlistUrl.contains("music.youtube.com")) {
             youtubeService.getPlaylistItems(playlistUrl)
         } else if (playlistUrl.contains("music.apple.com")) {
-            transferFromAppleMusic(playlistUrl)
-        }
-    }
 
-    private fun transferFromAppleMusic(playlistUrl: String) {
-        // TODO: Call Apple Music API: https://developer.apple.com/documentation/applemusicapi/
+            println("Apple Music transfer is not yet implemented.")
+            emptyList()
+        } else {
+            println("Unknown platform.")
+            emptyList()
+        }
     }
 }
